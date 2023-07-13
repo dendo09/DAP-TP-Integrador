ba04df7d750f232461ee2a39c8ad45049b54535c
dendo09 <126833423+dendo09@users.noreply.github.com>
2023-07-13 16:49:08 -0300

    final

--- app/src/main/java/adapter/TeamViewHolder.kt
+++ app/src/main/java/adapter/TeamViewHolder.kt
@@ -1,18 +1,22 @@
 package adapter
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.nbateams.Team
 import com.example.nbateams.R
 
-class TeamsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class TeamViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val txtTitle: TextView = itemView.findViewById(R.id.teamTitle)
     private val teamsLayout: ConstraintLayout = itemView.findViewById(R.id.teams_layout)
+    private val logoTeam = itemView.findViewById<ImageView>(R.id.imageView)
 
     fun bind(team: Team) {
         txtTitle.text = team.name
+        Glide.with(logoTeam.context).load(team.logo).into(logoTeam)
     }
 
     init {
--- app/src/main/java/com/example/nbateams/InfoFragment.kt
+++ app/src/main/java/com/example/nbateams/InfoFragment.kt
@@ -1,59 +1,40 @@
 package com.example.nbateams
 
+import adapter.TeamAdapter
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class InfoFragment : Fragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [infoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class infoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var sharedViewModel: SharedViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
-    }
+        val v = inflater.inflate(R.layout.fragment_info, container, false)
+        val textTitle = v.findViewById<TextView>(R.id.infoTitle)
+        val textConference = v.findViewById<TextView>(R.id.infoConference)
+        val textState = v.findViewById<TextView>(R.id.infoState)
+        val textMvp = v.findViewById<TextView>(R.id.infoMvp)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment infoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            infoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        sharedViewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+        val team = sharedViewModel.itemSelected
+
+        textTitle.text = team.name
+        textConference.text = team.conference
+        textState.text = team.state
+        textMvp.text = team.mvp
+
+        return v
     }
 }
+
